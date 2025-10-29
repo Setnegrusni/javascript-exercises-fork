015cc2f723f6504e430118186427e000d2543e26
Kenneth <krgutgar@gmail.com>
2025-10-28 20:52:31 -0600

    Exercise 10_leapYears finished

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divByFour = Number.isInteger(year / 4);
+    let divByHundred = Number.isInteger(year / 100);
+    let divByFourHundred = Number.isInteger(year / 400);
 
+    if ((divByFour && !divByHundred) || 
+        (!divByHundred && divByFourHundred) || 
+        (divByFour && divByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
