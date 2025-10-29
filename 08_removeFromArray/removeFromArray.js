9bb7a0a0d4ef422e02436aa29ea97554eea3f60a
Kenneth <krgutgar@gmail.com>
2025-10-28 19:39:37 -0600

    Exercise 08_removeFromArray finished

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...toRemove) {
+    //Filtra el array original para traer aquellos elementos no incluidos en "...toRemove"
+    //donde "...toRemove" es un array de argumentos
+    return myArray.filter(item => !toRemove.includes(item));
 };
 
 // Do not edit below this line
