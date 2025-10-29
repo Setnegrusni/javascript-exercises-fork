da198488d29634626789c8a8c34f48147b7d7aa2
Kenneth <krgutgar@gmail.com>
2025-10-28 20:30:51 -0600

    Exercise 09_sumAll finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
+    let sumNums = 0;
 
+    if (initial > final) {
+        let temp = initial;
+        initial = final;
+        final = temp;
+    }
+    if (initial < 0 || final < 0) return "ERROR";
+    if (!Number.isInteger(initial) || !Number.isInteger(final)) return "ERROR";
+
+    for (let i = initial; i <= final; i++) {
+        sumNums = sumNums + i;
+    }
+    return sumNums;
 };
 
 // Do not edit below this line
