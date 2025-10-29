e0b0c7f68de1d41302bc17534d6aeb2cb4f98a88
Kenneth <krgutgar@gmail.com>
2025-10-28 18:59:14 -0600

    Exercise 06_repeatString finished

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let fullStr = "";
 
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            fullStr = fullStr + str;
+        }
+        return fullStr;
+    } else {
+        return "ERROR";
+    }
 };
+
+console.log(repeatString("", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
