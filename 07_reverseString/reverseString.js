22221088891774596e8c57fc712f028609568a29
Kenneth <krgutgar@gmail.com>
2025-10-28 19:12:21 -0600

    Exercise 07_reverseString finished

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    charQty = str.length;
+    let reversedStr = "";
 
+    for(let i = charQty; i >= 0; i--) {
+        reversedStr = reversedStr + str.charAt(i);
+    }
+
+    return reversedStr;
 };
+
+//console.log(reverseString("123 ¡Hola, Mundo! 456"));
 
 // Do not edit below this line
 module.exports = reverseString;
