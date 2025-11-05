0a6eb9b52053bb7aabc4c98a2426a436c8c6dea4
Kenneth <krgutgar@gmail.com>
2025-11-04 19:46:30 -0600

    Exercise 12_calculator finished

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  return firstNum + secondNum; //correct
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum; //correct
 };
 
-const sum = function() {
-	
+const sum = function(myArr) {
+	return myArr.reduce((sumAll, actual) => (sumAll + actual), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(myArr) {
+ return myArr.reduce((multAll, actual) => (multAll * actual), 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+let fact = 1;
+
+  if (number < 0) {
+    return "ERROR"
+  } else {
+    if (number === 0 || number === 1) {
+      return 1
+    } else {
+      for (let i = 1; i <= number; i++) {
+        fact = fact * i;
+      }
+
+      return fact;
+    }
+  }
 };
 
 // Do not edit below this line
