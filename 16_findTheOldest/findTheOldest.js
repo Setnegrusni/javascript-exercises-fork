513d3623a07c400a684e317b8d191309249ebc35
Kenneth <krgutgar@gmail.com>
2025-11-05 20:17:02 -0600

    Exercise 16_findTheOldest finished

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(myPeopleArr) {
+    return oldest = myPeopleArr.reduce((a, b) => {
+        const personA = getAge(a.yearOfBirth, a.yearOfDeath);
+        const personB = getAge(b.yearOfBirth, b.yearOfDeath);
 
+        return personA < personB ? b : a;
+    })
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
